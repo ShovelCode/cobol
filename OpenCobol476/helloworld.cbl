98594ff84bf61528e5e574fdeb127b3b5a52af80
Christopher Ray <vde435@gmail.com>
2022-10-01 14:58:45 -0400

    Create helloworld.cbl

---
+++ OpenCobol476/helloworld.cbl
@@ -0,0 +1,13 @@
+      ******************************************************************
+      * Author: C.R.
+      * Date: 2022 Oct 1
+      * Purpose: Testing this IDE
+      * Tectonics: cobc
+      * 
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+
+       PROCEDURE DIVISION.
+       DISPLAY 'Hello World'.
+       STOP RUN.
